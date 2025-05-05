9fee7d0208bbec492038c3e1d70302a8772dde3f
firaol1223 <152330937+firaol1223@users.noreply.github.com>
2025-05-05 23:34:42 +0300

    Delete Codes/chapter1 directory

--- Codes/chapter1/Basic_Data_Types/3_signed-integers/Contract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    int8 public a = 26;
-    int8 public b = -78;
-    int16 public difference = a - b;
-}
--- Codes/chapter1/Functions/5_pure-double/Contract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    function double(uint _value) public pure returns(uint d) {
-        d = _value * 2;
-    }
-}
